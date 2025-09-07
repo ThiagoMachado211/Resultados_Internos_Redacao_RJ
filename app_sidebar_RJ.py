5eaf7af9243e39947db25023a9d37656dcf69d2e
ThiagoMachado211 <113355173+ThiagoMachado211@users.noreply.github.com>
2025-09-07 16:07:00 -0300

    Update app_sidebar_RJ.py

--- app_sidebar_RJ.py
+++ app_sidebar_RJ.py
@@ -220,7 +220,7 @@ tab_names = [
     if ("particip" not in n.lower()) and not ("texto" in n.lower() and "insuf" in n.lower())
 ]
 
-col_nav, col_main = st.columns([3, 2], gap="small")
+col_nav, col_main = st.columns([1, 2], gap="small")
 
 with col_nav:
     st.markdown("")
@@ -272,6 +272,7 @@ with col_main:
             ),
             use_container_width=True
         )
+
 
 
 
