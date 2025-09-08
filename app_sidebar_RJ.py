323a1e7c074da6c9b74b0d7a1027a53bb41166cb
ThiagoMachado211 <113355173+ThiagoMachado211@users.noreply.github.com>
2025-09-07 21:00:01 -0300

    Update app_sidebar_RJ.py

--- app_sidebar_RJ.py
+++ app_sidebar_RJ.py
@@ -26,7 +26,7 @@ st.markdown(
 html, body, [class*="css"] {{ font-size: {FONT_SIZE}px !important; }}
 .stSelectbox label {{ font-size: {FONT_SIZE}px !important; }}
 .stSelectbox div[data-baseweb="select"] div {{ font-size: {FONT_SIZE}px !important; }}
-div[role="radiogroup"] label, div[role="radiogroup"] <b> p </b> {{ font-size: 60px !important; color: blue; }}
+div[role="radiogroup"] label, div[role="radiogroup"] p {{ font-size: 60px !important; color: blue; font-weight: bold; }}
 
 /* respiro vertical nas opções do radio */
 div[role="radiogroup"] > * {{ margin-bottom: 25px !important; }}
@@ -333,6 +333,7 @@ with col_main:
             ),
             use_container_width=True
         )
+
 
 
 
